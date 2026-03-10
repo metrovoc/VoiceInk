f32761f2e5f9514aa485f56fa66b5beb5dafb25d
metrovoc <metrovoc@users.noreply.github.com>
2026-03-10 16:44:17 +0800

    Fix custom release signing and storage fallback

--- VoiceInk/AppDefaults.swift
+++ VoiceInk/AppDefaults.swift
@@ -1,4 +1,45 @@
 import Foundation
+import OSLog
+
+enum AppIdentity {
+    static let bundleIdentifier = "com.metrovoc.VoiceInk"
+    static let legacyBundleIdentifier = "com.prakashjoshipax.VoiceInk"
+    static let cloudKitContainerIdentifier = "iCloud.com.metrovoc.VoiceInk"
+    static let loggerSubsystem = "com.metrovoc.voiceink"
+}
+
+enum AppPaths {
+    static var applicationSupportDirectory: URL {
+        FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent(AppIdentity.bundleIdentifier, isDirectory: true)
+    }
+
+    static var legacyApplicationSupportDirectory: URL {
+        FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent(AppIdentity.legacyBundleIdentifier, isDirectory: true)
+    }
+
+    static var recordingsDirectory: URL {
+        applicationSupportDirectory.appendingPathComponent("Recordings", isDirectory: true)
+    }
+
+    static var modelsDirectory: URL {
+        applicationSupportDirectory.appendingPathComponent("WhisperModels", isDirectory: true)
+    }
+
+    static func ensureApplicationSupportDirectoryExists(logger: Logger) throws {
+        let fileManager = FileManager.default
+        let currentDirectory = applicationSupportDirectory
+        let legacyDirectory = legacyApplicationSupportDirectory
+
+        if !fileManager.fileExists(atPath: currentDirectory.path), fileManager.fileExists(atPath: legacyDirectory.path) {
+            try fileManager.moveItem(at: legacyDirectory, to: currentDirectory)
+            logger.notice("Migrated Application Support directory to \(currentDirectory.path, privacy: .public)")
+        }
+
+        try fileManager.createDirectory(at: currentDirectory, withIntermediateDirectories: true)
+    }
+}
 
 enum AppDefaults {
     static func registerDefaults() {
